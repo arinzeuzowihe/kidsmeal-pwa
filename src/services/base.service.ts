9da6bad4de14a0b8881626a1912476cfb1f1d001
arinzeuzowihe <deholeskool@yahoo.com>
2024-04-15 21:45:57 -0700

    Adds support for base api enviroment variable

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -1,7 +1,7 @@
 import { BaseKid, BaseUser } from "../interfaces/api/responses";
 import { ToastContainer, toast } from "react-toastify";
 
-const apiBaseUrl = "https://localhost:7136"
+const apiBaseUrl = (process.env.NODE_ENV === 'production') ? process.env.BASE_API_URL : "https://localhost:7136"
 
 class BaseService {
 
